a3f3624d1c4524c87783a2c0d4d5a7e2491b67c6
sanawulai <numbonayar@gmail.com>
2025-07-26 17:19:13 +0000

    instructor and instructor details added

--- src/main/java/com/sanawulai/crudedemo/CrudeDemeApplication.java
+++ src/main/java/com/sanawulai/crudedemo/CrudeDemeApplication.java
@@ -9,8 +9,7 @@ import org.springframework.context.annotation.Bean;
 public class CrudeDemeApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(CrudeDemeApplication.class, args);
-    }
+        SpringApplication.run(CrudeDemeApplication.class, args);}
 
 
     @Bean
